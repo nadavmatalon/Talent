aac904e04e67f8f86cfa8ebecdd8fd7814f0885e
Nadav <nadav.matalon@gmail.com>
2014-08-29 15:18:11 +0100

    general refactoring

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,15 +1,16 @@
 class ClientsController < ApplicationController
+
 before_action :authenticate_client!
 
 	def index
 	    @client = Client.find params[:id]
-	    @projects = @client.projects
-	    redirect_to '/' unless @client == current_client
+	    @projects = @client.projects.to_a.sort_by {|project| project.id}.reverse
+	    redirect_to root_path unless @client == current_client
 	end
 
 	def show
 	    @client = Client.find params[:id] 
-	    redirect_to '/' unless @client == current_client
+	    redirect_to root_path unless @client == current_client
 	end
 
 end
