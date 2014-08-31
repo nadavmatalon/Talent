b7f334bceffb410177e5df007406d181e2351d63
Nadav <nadav.matalon@gmail.com>
2014-08-31 18:52:00 +0100

    updated client sign up

--- app/controllers/clients/registrations_controller.rb
+++ app/controllers/clients/registrations_controller.rb
@@ -1,39 +1,42 @@
 class Clients::RegistrationsController < Devise::RegistrationsController
-  
-  def update
-    # For Rails 4
-    account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
-    # For Rails 3
-    # account_update_params = params[:client]
 
-    # required for settings form to submit when password is left blank
-    if account_update_params[:password].blank?      
-      account_update_params.delete("password")
-      account_update_params.delete("password_confirmation")
+    def update
+
+        account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
+
+        # required for settings form to submit when password is left blank
+        if account_update_params[:password].blank?
+            account_update_params.delete("password")
+            account_update_params.delete("password_confirmation")
+        end
+
+        if account_update_params[:email].blank?
+            account_update_params.delete("email")
+        end
+
+        if account_update_params[:current_password].blank?
+            account_update_params.delete("current_password")
+        end
+
+        @client = Client.find(current_client.id)
+
+        if @client.update_attributes(account_update_params)
+            sign_in @client, bypass: true
+            # redirect to show developer profile 
+            redirect_to client_path(@client), notice: "Profile successfully updated."          
+            # redirect to client dashboard 
+            # redirect_to client_dashboard_path(@client), notice: "Profile successfully updated."
+        else
+            render "edit"
+        end
     end
 
-    if account_update_params[:email].blank?      
-      account_update_params.delete("email")
+
+    private
+    
+    def after_update_path_for(resource)
+        client_path(resource)
     end
 
-    if account_update_params[:current_password].blank?      
-      account_update_params.delete("current_password")
-    end
-
-    @client = Client.find(current_client.id)
-    if @client.update_attributes(account_update_params)
-      set_flash_message :notice, :updated
-      # Sign in the user bypassing validation in case their password changed
-      # sign_in @client, bypass: true
-      sign_in_and_redirect @client,  bypass: true
-    else
-      render "edit"
-    end
-  end
-
-  protected
-
-  def after_update_path_for(resource)
-    client_path(resource)
-  end
 end
+
